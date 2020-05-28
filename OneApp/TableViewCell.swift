e4c38ab5344430317327b474eea9a9b173e66ca0
HondaNonoho <nnhhnd3012@gmail.com>
2020-05-28 19:33:53 +0900

    追加

---
+++ OneApp/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  OneApp
+//
+//  Created by nonoho.honda on 2020/05/28.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ OneApp/TableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  TableViewController.swift
+//  OneApp
+//
+//  Created by nonoho.honda on 2020/05/28.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var memoList = [MemoData] ()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     if let storedData = UserDefaults().data(forKey: "memoList") {
+            do {
+                let unarchivedData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(storedData)
+                memoList.append(contentsOf: unarchivedData as! [MemoData])
+            } catch {
+                print(error)
+            }
+        }
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return memoList.count
+    }
+
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+            
+        if indexPath.row == 0 {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath)
+        return cell
+          }else {
+        if indexPath.row == 1 {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SecondTableViewCell", for: indexPath)
+        return cell
+          }else {
+        if indexPath.row == 2 {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ThirdTableViewCell", for: indexPath)
+        return cell
+          }else {
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    return UITableViewCell()
+   }
+}
+}
+}
