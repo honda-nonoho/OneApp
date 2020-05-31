d77dfcafdfa6ba855dddadbf9d8492d45a924af3
HondaNonoho <nnhhnd3012@gmail.com>
2020-05-31 17:09:00 +0900

    完成　

---
+++ OneApp/AddMemoViewController.swift
@@ -0,0 +1,66 @@
+//
+//  AddMemoViewController.swift
+//  OneApp
+//
+//  Created by nonoho.honda on 2020/05/31.
+//  Copyright © 2020 nonoho.honda. All rights reserved.
+//
+
+import UIKit
+
+class AddMemoViewController: UIViewController {
+
+        @IBOutlet weak var memoTextView: UITextView!
+        @IBOutlet weak var FirstLabel: UILabel!
+        @IBOutlet weak var SecondLabel: UILabel!
+        @IBOutlet weak var ThirdLabel: UILabel!
+    
+    
+        override func viewDidLoad() {
+            super.viewDidLoad()
+        }
+
+        @IBAction func save(_ sender: Any) {
+
+            let inputText = memoTextView.text
+            let ud = UserDefaults.standard
+            if ud.array(forKey: "memoArray") != nil{
+                //saveMemoArrayに取得
+                var saveMemoArray = ud.array(forKey: "memoArray") as! [String]
+                    //テキストに何か書かれているか？
+                if inputText != ""{
+                    //配列に追加
+                    saveMemoArray.append(inputText!)
+                    ud.set(saveMemoArray, forKey: "memoArray")
+                }else{
+                    showAlert(title: "何も入力されていません")
+
+                }
+
+            }else{
+                //最初、何も書かれていない場合
+                var newMemoArray = [String]()
+                //nilを強制アンラップはエラーが出るから
+                if inputText != ""{
+                    //inputtextはoptional型だから強制アンラップ
+                    newMemoArray.append(inputText!)
+                    ud.set(newMemoArray, forKey: "memoArray")
+                }else{
+                    showAlert(title: "何も入力されていません")
+                }
+            }
+            showAlert(title: "保存完了")
+            ud.synchronize()
+        }
+
+        func showAlert(title:String){
+            let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+            alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
+
+            self.present(alert, animated: true, completion:nil)
+        }
+    }
+
--- OneApp/ViewController.swift
+++ OneApp/ViewController.swift
@@ -2,41 +2,78 @@
 //  ViewController.swift
 //  OneApp
 //
-//  Created by nonoho.honda on 2020/05/30.
+//  Created by nonoho.honda on 2020/05/31.
 //  Copyright © 2020 nonoho.honda. All rights reserved.
 //
 
 import UIKit
 
-//クラスの継承を追加
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-    //UITableView、numberOfRowsInSectionの追加(表示するcell数を決める)
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    @IBOutlet weak var memoTableView: UITableView!
+    var memoArray = [String]()
+
+    let ud = UserDefaults.standard
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //戻り値の設定(表示するcell数)
-        return MemoFile.count
+        return memoArray.count
     }
-    //UITableView、cellForRowAtの追加(表示するcellの中身を決める)
+
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //変数を作る
-        let MemoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MemoCell", for: indexPath)
-        //変数の中身を作る
-        MemoCell.textLabel!.text = MemoFile[indexPath.row]
-        //戻り値の設定（表示する中身)
-        return MemoCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+        cell.textLabel?.text = memoArray[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "toDetail", sender: nil)
+        //押したら押した状態を解除
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
-        //最初からあるコード
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            //追加画面で入力した内容を取得する
-            if UserDefaults.standard.object(forKey: "MemoList") != nil {
-                MemoFile = UserDefaults.standard.object(forKey: "MemoList") as! [String]
-            }
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-        //最初からあるコード
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
+        memoTableView.delegate = self
+        memoTableView.dataSource = self
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        loadMemo()
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //destinationのクラッシュ防ぐ
+        if segue.identifier == "toDetail"{
+            //detailViewControllerを取得
+            //as! DetailViewControllerでダウンキャストしている
+            let detailViewController = segue.destination as! DetailViewController
+            //遷移前に選ばれているCellが取得できる
+            let selectedIndexPath = memoTableView.indexPathForSelectedRow!
+            detailViewController.selectedMemo = memoArray[selectedIndexPath.row]
+            detailViewController.selectedRow = selectedIndexPath.row
         }
     }
+    func loadMemo(){
+        if ud.array(forKey: "memoArray") != nil{
+            //取得 またas!でアンラップしているのでnilじゃない時のみ
+            memoArray = ud.array(forKey: "memoArray") as![String]
+            //reloadしてくれる
+            memoTableView.reloadData()
+        }
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        if editingStyle == .delete {
+
+            //resultArray内のindexPathのrow番目をremove（消去）する
+            memoArray.remove(at: indexPath.row)
+
+            //再び
+//アプリ内に消去した配列を保存
+            ud.set(memoArray, forKey: "memoArray")
+
+            //tableViewを更新
+            tableView.reloadData()
+        }
+    }
+}
